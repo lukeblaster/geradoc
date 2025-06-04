c7a4125a7bb31bd7aea5d9699f32d02affdd019a
Lucas Soares <soaresl472@gmail.com>
2025-06-03 23:53:37 -0300

    styles: add mobile view and udpate some styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,90 +27,95 @@ export default function Home() {
   };
 
   return (
-    <div className="flex flex-col items-center p-3 justify-center h-full bg-base-200">
-      <div className="flex w-full justify-end items-end">
+    <div className="flex flex-col items-center p-3 justify-center min-h-full bg-base-200">
+      <div className="flex w-full justify-end items-end mb-3">
         <ThemeController />
       </div>
-      <div className="card flex flex-col h-full min-w-2/6 justify-center items-center">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            const formData = new FormData(e.currentTarget);
-            handleSubmit(formData);
-          }}
-          className="flex flex-col h-auto w-full justify-center p-6 rounded-md bg-base-100"
-        >
-          <div className="card-title gap-0.5 justify-center items-end text-center">
-            <h2 className="font-bold">GeraDOC</h2>
-            <span className="text-xs">by Lucas Silva</span>
-          </div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.currentTarget);
+          handleSubmit(formData);
+        }}
+        className="flex flex-col h-auto w-full md:max-w-2/3 lg:max-w-2/5 justify-center text-xs px-3 py-3 md:p-6 rounded-md bg-base-100"
+      >
+        <div className="card-title gap-0.5 justify-center items-end text-center mb-6">
+          <h2 className="font-bold">GeraDOC</h2>
+          <span className="text-xs">by Lucas Silva</span>
+        </div>
 
-          <fieldset className="fieldset">
-            <legend className="fieldset-legend">Templates</legend>
-            <div className="indicator">
-              <span className="indicator-item status status-success"></span>
-              <div className="px-3 py-2 border max-w-24 rounded-sm text-center text-xs">
-                Template 1
-              </div>
+        <fieldset className="fieldset">
+          <legend className="fieldset-legend">Templates </legend>
+          <div className="indicator">
+            <span className="indicator-item status status-success"></span>
+            <div className="px-3 py-2 border max-w-24 rounded-sm text-center text-xs">
+              Template 1
             </div>
-          </fieldset>
+          </div>
+          <a
+            href="https://github.com/lukeblaster/geradoc/"
+            target="_blank"
+            className="mt-0.5"
+          >
+            🔗 <span className="link">Ver templates</span>
+          </a>
+        </fieldset>
 
-          <fieldset className="fieldset">
-            <legend className="fieldset-legend">Nome do sistema</legend>
-            <input
-              type="text"
-              name="systemName"
-              placeholder="System Sistemas"
-              className="input w-full rounded-sm border-2 border-neutral"
-            />
-          </fieldset>
+        <fieldset className="fieldset">
+          <legend className="fieldset-legend">Nome do sistema</legend>
+          <input
+            type="text"
+            name="systemName"
+            placeholder="System Sistemas"
+            className="input w-full rounded-sm border-2 border-neutral"
+          />
+        </fieldset>
 
-          <fieldset className="fieldset">
-            <legend className="fieldset-legend">Nomes dos usuários</legend>
-            <textarea
-              className="textarea w-full rounded-sm max-h-32 border-2 border-neutral"
-              name="names"
-            />
-            <label className="label">
-              (Separado por vírgula. Ex: Lucas Silva, Carlos, Jonas Almeida)
-            </label>
-          </fieldset>
+        <fieldset className="fieldset">
+          <legend className="fieldset-legend">Nomes dos usuários</legend>
+          <textarea
+            className="textarea w-full rounded-sm max-h-32 border-2 border-neutral"
+            name="names"
+          />
+          <label className="label w-fit text-wrap">
+            (Separado por vírgula. Ex: Lucas Silva, Carlos, Jonas Almeida)
+          </label>
+        </fieldset>
 
-          <fieldset className="fieldset">
-            <legend className="fieldset-legend">Domínio</legend>
-            <input
-              type="text"
-              name="domain"
-              placeholder="@dominio.com.br"
-              className="input w-full rounded-sm border-2 border-neutral"
-            />
-          </fieldset>
+        <fieldset className="fieldset">
+          <legend className="fieldset-legend">Domínio</legend>
+          <input
+            type="text"
+            name="domain"
+            placeholder="@dominio.com.br"
+            className="input w-full rounded-sm border-2 border-neutral"
+          />
+        </fieldset>
 
-          <fieldset className="fieldset">
-            <legend className="fieldset-legend">Cor de fundo</legend>
-            <input
-              type="text"
-              name="color"
-              placeholder="#F2F2F2"
-              className="input w-full rounded-sm border-2 border-neutral"
-            />
-          </fieldset>
+        <fieldset className="fieldset">
+          <legend className="fieldset-legend">Cor de fundo</legend>
+          <input
+            type="text"
+            name="color"
+            placeholder="#F2F2F2"
+            className="input w-full rounded-sm border-2 border-neutral"
+          />
+        </fieldset>
 
-          <fieldset className="fieldset">
-            <legend className="fieldset-legend">Logo</legend>
-            <input
-              type="file"
-              accept="image/png"
-              name="logo"
-              className="file-input w-full rounded-sm"
-            />
-          </fieldset>
+        <fieldset className="fieldset">
+          <legend className="fieldset-legend">Logo</legend>
+          <input
+            type="file"
+            accept="image/png"
+            name="logo"
+            className="file-input w-full rounded-sm"
+          />
+        </fieldset>
 
-          <button type="submit" className="btn btn-primary mt-2 rounded-sm">
-            Gerar planilha
-          </button>
-        </form>
-      </div>
+        <button type="submit" className="btn btn-primary mt-2 rounded-sm">
+          Gerar planilha
+        </button>
+      </form>
     </div>
   );
 }
